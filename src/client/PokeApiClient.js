9bd2be9e9783b1249a75ff52f7d99dfd92d8f53a
Rolf <rolf@drivemarketing.ca>
2021-04-09 17:44:33 -0400

    simplify api client

--- src/client/PokeApiClient.js
+++ src/client/PokeApiClient.js
@@ -1,21 +1,6 @@
 export default class PokeApiClient {
-  constructor(url = 'https://pokeapi.co/api/v2/', limit = 12) {
-    this.limit = limit
-    this.url = url
-  }
 
-  async getPokemonsByPage(page = 1){
-    const offset = (page - 1) * this.limit
-    const url = `${this.url}pokemon?limit=${this.limit}&offset=${offset}`
-
-    const response = await fetch(url)
-    if(response.status >= 200 && response.status < 300){
-        return await response.json()
-    }
-    return {}
-  }
-
-  async getPokemonByUrl(url){
+  async get(url){
     if(!url) {
       return {}
     }
