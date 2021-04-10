81b7a68edb127caa1d6007ef2d38cdbbaad0a7f0
Rolf <rolf@drivemarketing.ca>
2021-04-10 15:23:42 -0400

    page navigation test

--- cypress/integration/pokedex.spec.js
+++ cypress/integration/pokedex.spec.js
@@ -12,5 +12,15 @@ describe('Pokédex e2e tests', () => {
 
       cy.get('[data-cy=title]').should('have.text', 'Pokédex')
     })
+
+    it(`visit page ${pageToVisit} of the pokedex`, () => {
+      cy.visit('http://localhost:3000')
+
+      cy.get(`[data-cy=link-${pageToVisit}]`).click()
+
+      cy.location().should((location) => {
+        expect(location.search).to.equal(`?page=${pageToVisit}`)
+      })
+    })
   })
 })
