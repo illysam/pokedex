3f9b489c37438103f8ff85bab102ea678b5c6ce8
Rolf <rolf@drivemarketing.ca>
2021-04-10 09:23:20 -0400

    use bootstrap classes

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,13 +3,13 @@
         <div class="card mb-4 box-shadow" v-if="pokemon !== undefined">
           <img class="card-img-top" :src="pokemon?.sprites?.front_default" :alt="pokemon.name">
           <div class="card-body">
-            <h5 class="card-title">{{ pokemon.name }}</h5>
-            <p class="card-text">Abilities</p>
+            <h5 class="card-title mb-4 text-uppercase font-weight-bold">{{ pokemon.name }}</h5>
+            <p class="card-text mb-1">Abilities</p>
             <ul>
-              <li v-for="ability in pokemon.abilities" :key="ability.slot" class="list-item">{{ ability.ability.name }}</li>
+              <li v-for="ability in pokemon.abilities" :key="ability.slot" class="list-item text-left">{{ ability.ability.name }}</li>
             </ul>
             <div class="d-flex justify-content-between align-items-center">
-                <a :href="`/pokemon/${pokemon.name}/`" class="btn btn-sm btn-outline-secondary">Details</a>
+                <router-link :to="{ name: 'pokemon', params: { name: pokemon.name }}" class="btn btn-sm btn-outline-secondary">Details</router-link>
                 <small class="text-muted">{{ pokemon.name }}-{{ pokemon.id ?? undefined }}</small>
             </div>
           </div>
