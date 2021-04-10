c8fa3cb370b364f02616e2561e13b04bf960a0c5
Rolf <rolf@drivemarketing.ca>
2021-04-09 20:41:19 -0400

    add router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store/store'
+import router from './router'
+import store from './store'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
+
 createApp(App)
+  .use(router)
   .use(store)
   .mount('#app')
