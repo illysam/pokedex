ad3ed08227e5babc94de50b365e5d3cd841f1299
Rolf <rolf@drivemarketing.ca>
2021-04-10 15:28:29 -0400

    page with evolves from

--- cypress/integration/pokedex.spec.js
+++ cypress/integration/pokedex.spec.js
@@ -26,6 +26,7 @@ describe('Pokédex e2e tests', () => {
 
   describe('pokemon page', () => {
     const pokemonName = 'bulbasaur'
+    const pokemonNameEvolves = 'ivysaur'
 
     it(`visit individual page for ${pokemonName}`, () => {
       cy.visit('http://localhost:3000')
@@ -34,10 +35,26 @@ describe('Pokédex e2e tests', () => {
 
       cy.get(`[data-cy=pokemon-${pokemonName}]`).click()
 
-      cy.get('[data-cy=title]').should('not.be.empty').should('have.text', pokemonName)
+      cy.get('[data-cy=title]').should('have.text', pokemonName)
 
       cy.location().should((location) => {
         expect(location.pathname).to.equal(`/pokemon/${pokemonName}/`)
+      })
+    })
+
+    it(`visit individual page for ${pokemonNameEvolves} with link to 'evolves from'`, () => {
+      cy.visit('http://localhost:3000')
+
+      console.log(`a[data-cy=pokemon-${pokemonNameEvolves}]`)
+
+      cy.get(`[data-cy=pokemon-${pokemonNameEvolves}]`).click()
+
+      cy.get('[data-cy=title]').should('have.text', pokemonNameEvolves)
+
+      cy.get('[data-cy=evolvesFrom]').should('have.text', pokemonName)
+
+      cy.location().should((location) => {
+        expect(location.pathname).to.equal(`/pokemon/${pokemonNameEvolves}/`)
       })
     })
   })
