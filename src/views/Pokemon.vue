ccae6b3c0e8ca6fa516301ef4738dbc93c7b5bb9
Rolf <rolf@drivemarketing.ca>
2021-04-10 09:56:30 -0400

    add more properties

--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -1,12 +1,51 @@
 <template>
   <main role="main">
-    <section class="jumbotron text-center">
+    <section class="jumbotron text-center mb-0">
       <div class="container">
         <p>
           <img class="image-fluid jumbotron-image-small border border-dark rounded" :src="pokemon?.sprites?.front_default" :alt="pokemon?.name" />
         </p>
         <h1 class="jumbotron-heading text-uppercase">{{ pokemon?.name }}</h1>
         <p class="lead text-muted">{{ pokemon?.name }}</p>
+        <div class="container w-50 d-flex justify-content-between">
+          <table class="table table-sm  table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Property</th>
+                <th scope="col">Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Species</th>
+                <td class="text-capitalize">{{ pokemon?.species.name }}</td>
+              </tr>
+              <tr>
+                <th scope="row">HP</th>
+                <td>{{ getStat('hp') }}</td>
+              </tr>
+              <tr>
+                <th scope="row">Attack</th>
+                <td>{{ getStat('attack') }}</td>
+              </tr>
+              <tr>
+                <th scope="row">Defense</th>
+                <td>{{ getStat('defense') }}</td>
+              </tr>
+              <tr>
+                <th scope="row">Height</th>
+                <td>{{ pokemon?.height }}</td>
+              </tr>
+              <tr>
+                <th scope="row">Weight</th>
+                <td>{{ pokemon?.weight }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="mt-5">
+          <router-link class="btn btn-primary" role="button" :to="{name:'main', query: {page: 1}}">Back to list</router-link>
+        </div>
       </div>
     </section>
   </main>
@@ -24,5 +63,10 @@ export default {
       return this.$store.state.pokemons.get(this.$route.params.name)
     }
   },
+  methods: {
+    getStat: function(name){
+      return this.pokemon?.stats.find((stat) => stat.stat.name === name).base_stat ?? 'na'
+    }
+  }
 }
 </script>
